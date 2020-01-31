9caeb5e0b0a1913b17c515af39772e1902400518
Andrew Bates <abatesaccs@gmail.com>
2020-01-31 15:47:01 -0800

    update startup to work with jwt

--- Startup.cs
+++ Startup.cs
@@ -8,8 +8,8 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
 using System.Text;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
-// using LocalBusinessLookup.Helpers;
-// using LocalBusinessLookup.Services;
+using LocalBusinessLookup.Helpers;
+using LocalBusinessLookup.Services;
 
 namespace LocalBusinessLookup
 {
@@ -26,7 +26,33 @@ namespace LocalBusinessLookup
         {
             services.AddDbContext<LocalBusinessLookupContext>(opt =>
                 opt.UseMySql(Configuration.GetConnectionString("DefaultConnection")));
+            services.AddCors();
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+
+            var appSettingsSection = Configuration.GetSection("AppSettings");
+            services.Configure<AppSettings>(appSettingsSection);
+
+            var appSettings = appSettingsSection.Get<AppSettings>();
+            var key = Encoding.ASCII.GetBytes(appSettings.Secret);
+            services.AddAuthentication(x =>
+            {
+                x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            })
+            .AddJwtBearer(x =>
+            {
+                x.RequireHttpsMetadata = false;
+                x.SaveToken = true;
+                x.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuerSigningKey = true,
+                    IssuerSigningKey = new SymmetricSecurityKey(key),
+                    ValidateIssuer = false,
+                    ValidateAudience = false
+                };
+            });
+
+            services.AddScoped<IUserService, UserService>();
         }
 
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
@@ -39,6 +65,13 @@ namespace LocalBusinessLookup
             {
                 app.UseHsts();
             }
+
+            app.UseCors(x => x
+                .AllowAnyOrigin()
+                .AllowAnyMethod()
+                .AllowAnyHeader());
+
+            app.UseAuthentication();
 
             //app.UseHttpsRedirection();
             app.UseMvc();
