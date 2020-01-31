975eff7a822068a8ab8a460d5624bcedf6735d42
Andrew Bates <abatesaccs@gmail.com>
2020-01-31 13:30:43 -0800

    create models and launch settings json

---
+++ Models/LocalBusinessLookupContext.cs
@@ -0,0 +1,22 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace LocalBusinessLookup.Models
+{
+    public class LocalBusinessLookupContext : DbContext
+    {
+        public LocalBusinessLookupContext(DbContextOptions<LocalBusinessLookupContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Business> Businesses { get; set; }
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<Business>()
+                .HasData(
+                    new Business { BusinessId = 1, Name = "Dave's Music", Address = "123 N Street St", PhoneNumber = "7", Website = "Example.com" },
+                    new Business { BusinessId = 2, Name = "John's Sounds", Address = "456 N Way Rd", PhoneNumber = "867-5309", Website = "Example.com" }
+                );
+        } 
+    }
+}
