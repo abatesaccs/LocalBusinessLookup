89b116e3ddff11ddeb16ed280d8ff86bd0ade58b
Student <example@example.com>
2020-02-10 16:57:34 -0800

    fix user requirement

--- Entities/User.cs
+++ Entities/User.cs
@@ -5,9 +5,7 @@ namespace LocalBusinessLookup.Entities
         public int Id { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
-        [Required]
         public string Username { get; set; }
-        [Required]
         public string Password { get; set; }
         public string Token { get; set; }
     }
